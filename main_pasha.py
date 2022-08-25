79e200e9fe5166d7e792ee2dde204e130ef95340
REDMemphis <112066265+REDMemphis@users.noreply.github.com>
2022-08-25 13:00:52 +0300

    Update main_pasha.py

--- main_pasha.py
+++ main_pasha.py
@@ -1,1 +1,21 @@
-Проверка
+def get_together_games(games_1, games_2)
+        return together_games
+set(games_1).intersection(set(games_2))
+anfisa_games = [
+    'Online-chess',
+    'Города',
+    'DOOM',
+    'Крестики-нолики'
+]
+alisa_games = [
+    'DOOM',
+    'Online-chess',
+    'Города',
+    'GTA',
+    'World of tanks'
+]
+# Вызовите функцию со списками игр в качестве параметров
+together_games = ...
+# Напечатайте итоговый перечень игр в цикле
+for game in together_games:
+    print(...)
